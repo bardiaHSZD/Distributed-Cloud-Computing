42139f6f011efde51ae04a8d361c0491b86fd456
bardia@ualberta.ca <bardia@ualberta.ca>
2024-06-01 05:38:43 -0700

    Update Lab-1 and Summary

---
+++ Generative AI with Large Language Models/Week 1 - Generative AI use cases, project lifecycle, and model pre-training/summary.py
@@ -0,0 +1,137 @@
+# ======== Libraries ===========================================================
+import os
+
+instance_type_expected = 'ml-m5-2xlarge'
+instance_type_current = os.environ.get('HOSTNAME')
+
+print(f'Expected instance type: instance-datascience-{instance_type_expected}')
+print(f'Currently chosen instance type: {instance_type_current}')
+
+assert instance_type_expected in instance_type_current, f'ERROR. You selected the {instance_type_current} instance type. Please select {instance_type_expected} instead as shown on the screenshot above'
+print("Instance type has been chosen correctly.")
+
+%pip install -U datasets==2.17.0
+
+%pip install --upgrade pip
+%pip install --disable-pip-version-check \
+    torch==1.13.1 \
+    torchdata==0.5.1 --quiet
+
+%pip install \
+    transformers==4.27.2 --quiet
+
+from datasets import load_dataset
+from transformers import AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
+from transformers import GenerationConfig
+
+
+# ======== Database ===========================================================
+
+
+huggingface_dataset_name = "knkarthick/dialogsum"
+
+dataset = load_dataset(huggingface_dataset_name)
+   
+       
+# ======== Flan-T5 Model ===========================================================
+
+model_name='google/flan-t5-base'
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name)    
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+  
+    
+# ======== Make Prompt ===========================================================
+
+def make_prompt(example_indices_full, example_index_to_summarize):
+    prompt = ''
+    for index in example_indices_full:
+        dialogue = dataset['test'][index]['dialogue']
+        summary = dataset['test'][index]['summary']
+        
+        # The stop sequence '{summary}\n\n\n' is important for FLAN-T5. Other models may have their own preferred stop sequence.
+        prompt += f"""
+Dialogue:
+
+{dialogue}
+
+What was going on?
+{summary}
+
+
+"""
+    
+    dialogue = dataset['test'][example_index_to_summarize]['dialogue']
+    
+    prompt += f"""
+Dialogue:
+
+{dialogue}
+
+What was going on?
+"""
+        
+    return prompt    
+
+
+# ======== Few-Shot Inference ===========================================================
+
+example_indices_full = [40, 80, 120]
+example_index_to_summarize = 200
+
+few_shot_prompt = make_prompt(example_indices_full, example_index_to_summarize)
+
+print(few_shot_prompt)
+
+
+summary = dataset['test'][example_index_to_summarize]['summary']
+
+inputs = tokenizer(few_shot_prompt, return_tensors='pt')
+output = tokenizer.decode(
+    model.generate(
+        inputs["input_ids"],
+        max_new_tokens=50,
+    )[0], 
+    skip_special_tokens=True
+)
+
+dash_line = '-'.join('' for x in range(100))
+
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
+print(dash_line)
+print(f'MODEL GENERATION - FEW SHOT:\n{output}')
+
+
+# ======== Generative Configuration Parameters for Inference ===========================================================
+
+
+# generation_config = GenerationConfig(max_new_tokens=50)
+# generation_config = GenerationConfig(max_new_tokens=10)
+# generation_config = GenerationConfig(max_new_tokens=50, do_sample=True, temperature=0.1)
+generation_config = GenerationConfig(max_new_tokens=50, do_sample=True, temperature=0.5)
+# generation_config = GenerationConfig(max_new_tokens=50, do_sample=True, temperature=1.0)
+
+inputs = tokenizer(few_shot_prompt, return_tensors='pt')
+output = tokenizer.decode(
+    model.generate(
+        inputs["input_ids"],
+        generation_config=generation_config,
+    )[0], 
+    skip_special_tokens=True
+)
+
+print(dash_line)
+print(f'MODEL GENERATION - FEW SHOT:\n{output}')
+print(dash_line)
+print(f'BASELINE HUMAN SUMMARY:\n{summary}\n')
+
+"""
+Comments related to the choice of the parameters in the code cell above:
+
+    Choosing max_new_tokens=10 will make the output text too short, so the dialogue summary will be cut.
+    Putting do_sample = True and changing the temperature value you get more flexibility in the output.
+
+"""
