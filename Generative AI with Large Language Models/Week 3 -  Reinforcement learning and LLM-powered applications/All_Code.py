e1fe6f600769679f98f184eb9bd0c77a7e337ff1
bardia@ualberta.ca <bardia@ualberta.ca>
2024-06-02 02:34:00 -0700

    Upload Lab 3

---
+++ Generative AI with Large Language Models/Week 3 -  Reinforcement learning and LLM-powered applications/All_Code.py
@@ -0,0 +1,398 @@
+# ========= Libraries =====================================================
+%pip install --upgrade pip
+%pip install --disable-pip-version-check \
+    torch==1.13.1 \
+    torchdata==0.5.1 --quiet
+
+%pip install \
+    transformers==4.27.2 \
+    datasets==2.11.0 \
+    evaluate==0.4.0 \
+    rouge_score==0.1.2 \
+    peft==0.3.0 --quiet
+
+# Installing the Reinforcement Learning library directly from github.
+%pip install git+https://github.com/lvwerra/trl.git@25fa1bd 
+
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification, AutoModelForSeq2SeqLM, GenerationConfig
+from datasets import load_dataset
+from peft import PeftModel, PeftConfig, LoraConfig, TaskType
+
+# trl: Transformer Reinforcement Learning library
+from trl import PPOTrainer, PPOConfig, AutoModelForSeq2SeqLMWithValueHead
+from trl import create_reference_model
+from trl.core import LengthSampler
+
+import torch
+import evaluate
+
+import numpy as np
+import pandas as pd
+
+# tqdm library makes the loops show a smart progress meter.
+from tqdm import tqdm
+tqdm.pandas()
+
+# ========= Dataset =====================================================
+model_name="google/flan-t5-base"
+huggingface_dataset_name = "knkarthick/dialogsum"
+
+dataset_original = load_dataset(huggingface_dataset_name)
+
+
+def build_dataset(model_name,
+                  dataset_name,
+                  input_min_text_length, 
+                  input_max_text_length):
+
+    """
+    Preprocess the dataset and split it into train and test parts.
+
+    Parameters:
+    - model_name (str): Tokenizer model name.
+    - dataset_name (str): Name of the dataset to load.
+    - input_min_text_length (int): Minimum length of the dialogues.
+    - input_max_text_length (int): Maximum length of the dialogues.
+        
+    Returns:
+    - dataset_splits (datasets.dataset_dict.DatasetDict): Preprocessed dataset containing train and test parts.
+    """
+    
+    # load dataset (only "train" part will be enough for this lab).
+    dataset = load_dataset(dataset_name, split="train")
+    
+    # Filter the dialogues of length between input_min_text_length and input_max_text_length characters.
+    dataset = dataset.filter(lambda x: len(x["dialogue"]) > input_min_text_length and len(x["dialogue"]) <= input_max_text_length, batched=False)
+
+    # Prepare tokenizer. Setting device_map="auto" allows to switch between GPU and CPU automatically.
+    tokenizer = AutoTokenizer.from_pretrained(model_name, device_map="auto")
+    
+    def tokenize(sample):
+        
+        # Wrap each dialogue with the instruction.
+        prompt = f"""
+Summarize the following conversation.
+
+{sample["dialogue"]}
+
+Summary:
+"""
+        sample["input_ids"] = tokenizer.encode(prompt)
+        
+        # This must be called "query", which is a requirement of our PPO library.
+        sample["query"] = tokenizer.decode(sample["input_ids"])
+        return sample
+
+    # Tokenize each dialogue.
+    dataset = dataset.map(tokenize, batched=False)
+    dataset.set_format(type="torch")
+    
+    # Split the dataset into train and test parts.
+    dataset_splits = dataset.train_test_split(test_size=0.2, shuffle=False, seed=42)
+
+    return dataset_splits
+
+dataset = build_dataset(model_name=model_name,
+                        dataset_name=huggingface_dataset_name,
+                        input_min_text_length=200, 
+                        input_max_text_length=1000)
+
+# ========= PEFT Model =====================================================
+
+!aws s3 cp --recursive s3://dlai-generative-ai/models/peft-dialogue-summary-checkpoint/ ./peft-dialogue-summary-checkpoint-from-s3/ 
+!ls -alh ./peft-dialogue-summary-checkpoint-from-s3/adapter_model.bin
+
+def print_number_of_trainable_model_parameters(model):
+    trainable_model_params = 0
+    all_model_params = 0
+    for _, param in model.named_parameters():
+        all_model_params += param.numel()
+        if param.requires_grad:
+            trainable_model_params += param.numel()
+    return f"\ntrainable model parameters: {trainable_model_params}\nall model parameters: {all_model_params}\npercentage of trainable model parameters: {100 * trainable_model_params / all_model_params:.2f}%"
+
+
+lora_config = LoraConfig(
+    r=32, # Rank
+    lora_alpha=32,
+    target_modules=["q", "v"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type=TaskType.SEQ_2_SEQ_LM # FLAN-T5
+)
+
+model = AutoModelForSeq2SeqLM.from_pretrained(model_name, 
+                                              torch_dtype=torch.bfloat16)
+
+peft_model = PeftModel.from_pretrained(model, 
+                                       './peft-dialogue-summary-checkpoint-from-s3/', 
+                                       lora_config=lora_config,
+                                       torch_dtype=torch.bfloat16, 
+                                       device_map="auto",                                       
+                                       is_trainable=True)
+
+print(f'PEFT model parameters to be updated:\n{print_number_of_trainable_model_parameters(peft_model)}\n')
+
+
+# ========= PPO Model =====================================================
+ppo_model = AutoModelForSeq2SeqLMWithValueHead.from_pretrained(peft_model,                                                               
+                                                               torch_dtype=torch.bfloat16,
+                                                               is_trainable=True)
+
+print(f'PPO model parameters to be updated (ValueHead + 769 params):\n{print_number_of_trainable_model_parameters(ppo_model)}\n')
+print(ppo_model.v_head)
+
+ref_model = create_reference_model(ppo_model)
+
+print(f'Reference model parameters to be updated:\n{print_number_of_trainable_model_parameters(ref_model)}\n')
+
+# ========= Reward Model =====================================================
+
+toxicity_model_name = "facebook/roberta-hate-speech-dynabench-r4-target"
+toxicity_tokenizer = AutoTokenizer.from_pretrained(toxicity_model_name, device_map="auto")
+toxicity_model = AutoModelForSequenceClassification.from_pretrained(toxicity_model_name, device_map="auto")
+print(toxicity_model.config.id2label)
+
+"""
+on_toxic_text = "#Person 1# tells Tommy that he didn't like the movie."
+
+toxicity_input_ids = toxicity_tokenizer(non_toxic_text, return_tensors="pt").input_ids
+
+logits = toxicity_model(input_ids=toxicity_input_ids).logits
+print(f'logits [not hate, hate]: {logits.tolist()[0]}')
+
+# Print the probabilities for [not hate, hate]
+probabilities = logits.softmax(dim=-1).tolist()[0]
+print(f'probabilities [not hate, hate]: {probabilities}')
+
+# get the logits for "not hate" - this is the reward!
+not_hate_index = 0
+nothate_reward = (logits[:, not_hate_index]).tolist()
+print(f'reward (high): {nothate_reward}')
+"""
+
+"""
+# Output:
+logits [not hate, hate]: [-0.6921188831329346, 0.3722729980945587]
+probabilities [not hate, hate]: [0.25647106766700745, 0.7435289621353149]
+reward (low): [-0.6921188831329346]
+
+"""
+
+
+"""
+device = 0 if torch.cuda.is_available() else "cpu"
+
+sentiment_pipe = pipeline("sentiment-analysis", 
+                          model=toxicity_model_name, 
+                          device=device)
+reward_logits_kwargs = {
+    "top_k": None, # Return all scores.
+    "function_to_apply": "none", # Set to "none" to retrieve raw logits.
+    "batch_size": 16
+}
+
+reward_probabilities_kwargs = {
+    "top_k": None, # Return all scores.
+    "function_to_apply": "softmax", # Set to "softmax" to apply softmax and retrieve probabilities.
+    "batch_size": 16
+}
+
+print("Reward model output:")
+print("For non-toxic text")
+print(sentiment_pipe(non_toxic_text, **reward_logits_kwargs))
+print(sentiment_pipe(non_toxic_text, **reward_probabilities_kwargs))
+print("For toxic text")
+print(sentiment_pipe(toxic_text, **reward_logits_kwargs))
+print(sentiment_pipe(toxic_text, **reward_probabilities_kwargs))
+
+"""
+
+"""
+# Output:
+Reward model output:
+For non-toxic text
+[{'label': 'nothate', 'score': 3.114100694656372}, {'label': 'hate', 'score': -2.4896175861358643}]
+[{'label': 'nothate', 'score': 0.9963293671607971}, {'label': 'hate', 'score': 0.003670616541057825}]
+For toxic text
+[{'label': 'hate', 'score': 0.3722729980945587}, {'label': 'nothate', 'score': -0.6921188831329346}]
+[{'label': 'hate', 'score': 0.7435289621353149}, {'label': 'nothate', 'score': 0.25647106766700745}]
+"""
+
+# ========= Toxicity Evaluation =====================================================
+
+def evaluate_toxicity(model, 
+                      toxicity_evaluator, 
+                      tokenizer, 
+                      dataset, 
+                      num_samples):
+    
+    """
+    Preprocess the dataset and split it into train and test parts.
+
+    Parameters:
+    - model (trl model): Model to be evaluated.
+    - toxicity_evaluator (evaluate_modules toxicity metrics): Toxicity evaluator.
+    - tokenizer (transformers tokenizer): Tokenizer to be used.
+    - dataset (dataset): Input dataset for the evaluation.
+    - num_samples (int): Maximum number of samples for the evaluation.
+        
+    Returns:
+    tuple: A tuple containing two numpy.float64 values:
+    - mean (numpy.float64): Mean of the samples toxicity.
+    - std (numpy.float64): Standard deviation of the samples toxicity.
+    """
+
+    max_new_tokens=100
+
+    toxicities = []
+    input_texts = []
+    for i, sample in tqdm(enumerate(dataset)):
+        input_text = sample["query"]
+
+        if i > num_samples:
+            break
+            
+        input_ids = tokenizer(input_text, return_tensors="pt", padding=True).input_ids
+        
+        generation_config = GenerationConfig(max_new_tokens=max_new_tokens,
+                                             tok_k=0.0,
+                                             top_p=1.0,
+                                             do_sample=True)
+
+        response_token_ids = model.generate(input_ids=input_ids,
+                                            generation_config=generation_config)
+        
+        generated_text = tokenizer.decode(response_token_ids[0], skip_special_tokens=True)
+        
+        toxicity_score = toxicity_evaluator.compute(predictions=[(input_text + " " + generated_text)])
+
+        toxicities.extend(toxicity_score["toxicity"])
+
+    # Compute mean & std using np.
+    mean = np.mean(toxicities)
+    std = np.std(toxicities)
+        
+    return mean, std
+
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, device_map="auto")
+
+mean_before_detoxification, std_before_detoxification = evaluate_toxicity(model=ref_model, 
+                                                                          toxicity_evaluator=toxicity_evaluator, 
+                                                                          tokenizer=tokenizer, 
+                                                                          dataset=dataset["test"], 
+                                                                          num_samples=10)
+
+print(f'toxicity [mean, std] before detox: [{mean_before_detoxification}, {std_before_detoxification}]')
+
+"""
+toxicity [mean, std] before detox: [0.027250663118055938, 0.03593675945422879]
+
+"""
+
+# =========  Initialize PPOTrainer =====================================================
+def collator(data):
+    return dict((key, [d[key] for d in data]) for key in data[0])
+
+test_data = [{"key1": "value1", "key2": "value2", "key3": "value3"}]
+print(f'Collator input: {test_data}')
+print(f'Collator output: {collator(test_data)}')
+"""
+Collator input: [{'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}]
+Collator output: {'key1': ['value1'], 'key2': ['value2'], 'key3': ['value3']}
+
+"""
+
+learning_rate=1.41e-5
+max_ppo_epochs=1
+mini_batch_size=4
+batch_size=16
+
+config = PPOConfig(
+    model_name=model_name,    
+    learning_rate=learning_rate,
+    ppo_epochs=max_ppo_epochs,
+    mini_batch_size=mini_batch_size,
+    batch_size=batch_size
+)
+
+ppo_trainer = PPOTrainer(config=config, 
+                         model=ppo_model, 
+                         ref_model=ref_model, 
+                         tokenizer=tokenizer, 
+                         dataset=dataset["train"], 
+                         data_collator=collator)
+
+
+# ========= Fine-Tune the Model =====================================================
+"""
+The fine-tuning loop consists of the following main steps:
+
+    Get the query responses from the policy LLM (PEFT model).
+    Get sentiments for query/responses from hate speech RoBERTa model.
+    Optimize policy with PPO using the (query, response, reward) triplet.
+
+The operation is running if you see the following metrics appearing:
+
+    objective/kl: minimize kl divergence,
+    ppo/returns/mean: maximize mean returns,
+    ppo/policy/advantages_mean: maximize advantages.
+
+"""
+output_min_length = 100
+output_max_length = 400
+output_length_sampler = LengthSampler(output_min_length, output_max_length)
+
+generation_kwargs = {
+    "min_length": 5,
+    "top_k": 0.0,
+    "top_p": 1.0,
+    "do_sample": True
+}
+
+reward_kwargs = {
+    "top_k": None, # Return all scores.
+    "function_to_apply": "none", # You want the raw logits without softmax.
+    "batch_size": 16
+}
+
+max_ppo_steps = 10
+
+for step, batch in tqdm(enumerate(ppo_trainer.dataloader)):
+    # Break when you reach max_steps.
+    if step >= max_ppo_steps:
+        break   
+
+    prompt_tensors = batch["input_ids"]
+
+    # Get response from FLAN-T5/PEFT LLM.
+    summary_tensors = []
+
+    for prompt_tensor in prompt_tensors:
+        max_new_tokens = output_length_sampler()        
+            
+        generation_kwargs["max_new_tokens"] = max_new_tokens
+        summary = ppo_trainer.generate(prompt_tensor, **generation_kwargs)
+        
+        summary_tensors.append(summary.squeeze()[-max_new_tokens:])
+        
+    # This needs to be called "response".
+    batch["response"] = [tokenizer.decode(r.squeeze()) for r in summary_tensors]
+
+    # Compute reward outputs.
+    query_response_pairs = [q + r for q, r in zip(batch["query"], batch["response"])]    
+    rewards = sentiment_pipe(query_response_pairs, **reward_kwargs)
+
+    # You use the `nothate` item because this is the score for the positive `nothate` class.
+    reward_tensors = [torch.tensor(reward[not_hate_index]["score"]) for reward in rewards]    
+
+    # Run PPO step.
+    stats = ppo_trainer.step(prompt_tensors, summary_tensors, reward_tensors)
+    ppo_trainer.log_stats(stats, batch, reward_tensors)
+    
+    print(f'objective/kl: {stats["objective/kl"]}')
+    print(f'ppo/returns/mean: {stats["ppo/returns/mean"]}')
+    print(f'ppo/policy/advantages_mean: {stats["ppo/policy/advantages_mean"]}')
+    print('-'.join('' for x in range(100)))
+
